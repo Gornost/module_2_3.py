7c750c5135812e8549b462fdc4eb43aa781a3a3a
Gornost <nasta.g80@mail.ru>
2024-09-15 01:02:50 +0900

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,14 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+my_list[0] = 42
+my_list[-1][0] = 5
+while true:
+    number = int(input('введите число: '))
+    if number > 0:
+        print('my_list')
+        continue
+    if number <= 0:
+        break
+    else:
+        print()
+
+
